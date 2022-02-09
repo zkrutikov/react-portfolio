946ef1a454268d81bfa7994562a594af52bf6cbf
Zak Krutikov <zak.krutikov@gmail.com>
2022-02-08 23:11:38 -0500

    finished resume component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col} from "react-bootstrap";
 
 function Footer() {
-  <div className="footer">
-    <p>This is some content in sticky footer</p>
-  </div>;
+  <Row className="justify-content-md-center">
+      <Col xs={12} sm={4} md={4}>
+      <footer>Hey there!</footer>
+    </Col>
+      </Row>
 }
 
 export default Footer;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function Resume() {
   return (
     <Container>
-      <h1>Resume Page</h1>
+      <h1>Skills</h1>
+        <h2>Front end:</h2>
+         <p>JavaScript, React, HTML5, CSS, Styled components.</p>
+        <h2>Back end:</h2>
+         <p>Node.js, Express.js, MongoDB, Mongoose</p>
+        <h2>DevOps:</h2>
+         <p>Git, Github, Heroku, React Dev Tools</p>
+         <Button variant="outline-dark" href="https://www.linkedin.com/in/zakhar-krutikov-82270654/">LinkedIn</Button> <Button variant="outline-dark" href="https://github.com/zkrutikov">Github</Button>
     </Container>
   );
 }
